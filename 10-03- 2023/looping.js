f29d3ea57b81b21b2016d0c3763d48474aba3b71
Danar Zulfian Wirakusumah <127115867+Danarzlf@users.noreply.github.com>
2023-03-12 19:17:38 +0700

    Add files via upload

---
+++ 10-03- 2023/looping.js
@@ -0,0 +1,40 @@
+// ITERATION
+while loop
+
+// let number = 1
+// while (number <= 100) {
+//     if (number % 5 === 0) {
+//     console.log(number)
+//     }
+//     number += 1
+// }
+
+
+
+//for loop
+for(let i = 1; i <= 100; i++) {
+    if (i % 5 === 0) {
+        console.log("ini angka-" + i);
+}
+}
+
+//jadi pas kelipatan 3 kecetak nya ini kelipatan 3
+for(let i = 1; i <= 100; i++) {
+    if (i % 5 === 0) {
+        console.log("ini kelipatan 3");
+    } else {
+        console.log(i);
+    }
+}
+
+
+for (let i = 1; i >= 100; i++) {
+    if(i % 5 === 0) {
+        console.log(i);
+    }
+}
+
+
+
+
+
